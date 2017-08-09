e7cb70e75b005d20267fe6a3e3a769ec0fa1af03
Andreas Bresser <andreasbresser@gmail.com>
2017-08-09 18:07:31 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -19,15 +19,5 @@ echo "SOLR-Server IP:" $SOLR_IP
 echo "MySQL root password:" $MYSQL_ROOT
 echo "MySQL user password:" $MYSQL_USER
 
-echo "Upgrade LocalSettings.php"
-sed -i 's/\$wgDBpassword.*/\$wgDBpassword = "'$MYSQL_USER'";/g' src/LocalSettings.php
-sed -i 's/\$wgDBserver.*/\$wgDBserver = "'$MYSQL_IP'";/g' src/LocalSettings.php
-
-SECRET_KEY=`head -c32 </dev/urandom|xxd -p -u -c64`
-sed -i 's/\$wgSecretKey.*/\$wgSecretKey = "'$SECRET_KEY'";/g' src/LocalSettings.php
-
-UPGRADE_KEY=`head -c8 </dev/urandom|xxd -p -u`
-sed -i 's/\$wgUpgradeKey.*/\$wgUpgradeKey = "'$UPGRADE_KEY'";/g' src/LocalSettings.php
-
 echo "update database"
 sudo docker exec -it mysql-mediawiki mysql -u root -p$MYSQL_ROOT -e "CREATE DATABASE mediawiki; CREATE USER 'wikiuser'@'%'  IDENTIFIED BY '$MYSQL_USER'; GRANT ALL ON mediawiki.* TO 'wikiuser'@'%'"
